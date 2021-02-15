54f0261bdf683a5f360599ae1c8e7cf02c06ca26
ianmcnicholas <ianjmcnicholas@gmail.com>
2021-02-15 17:12:22 +0000

    Sinatra:Views - separating HTML code from the ruby code

--- app.rb
+++ app.rb
@@ -12,7 +12,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-   <img src='https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/18/22/18-22-301P__CENTER/Sneaky-Cat-Laura-Graves-Canvas-Print.jpg'>
- </div>"
+  erb(:index)
 end
