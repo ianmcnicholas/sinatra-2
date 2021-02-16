e8980ee30dfad25dccbb41976523e6f58fd0d227
ianmcnicholas <ianjmcnicholas@gmail.com>
2021-02-16 14:47:02 +0000

    Sinatra: Introducing params. allowing user to add name to cat in get request

--- app.rb
+++ app.rb
@@ -11,10 +11,15 @@ get '/secret' do
   "Shhhhhhhhhhhh I'm hunting duckies"
 end
 
-get '/cat' do
+get '/random-cat' do
   @catname = %w(Amigo Misty Almond).sample
   erb(:index)
 end
 
+get '/named-cat' do
+  p params
+  @catname = params[:name]
+  erb(:index)
+end
 
 #test comment
